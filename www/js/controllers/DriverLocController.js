144930f46bbe7443d71fd466401d810329947266
kimo5532 <shotbyabel@gmail.com>
2015-11-23 18:34:12 -0800

    fix geoLocation and add navigator method

--- www/js/controllers/DriverLocController.js
+++ www/js/controllers/DriverLocController.js
@@ -5,6 +5,21 @@
 angular.module('starter')
   .controller("DriverLocCtrl", function($scope) {
 
+    // function showMap(coords) {
+    //   var mapOptions = {
+    //     center: {lat: coords.latitude, lng: coords.longitue},
+    //     zoom: 8
+    //   };
+    //   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    // }
+
+    // driverLocationService.getPosition()
+    //   .then(function(position) {
+    //     $scope.coords = position.coords;
+    //     showMap(position.coords);
+    //   }, function(err) {
+    //     console.log('getCurrentPosition error: ' + angular.toJson(err));
+    //   });
 
 });
 
