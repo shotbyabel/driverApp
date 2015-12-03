d665887f8234ec6ea193c8309013e4a9016eb242
kimo5532 <shotbyabel@gmail.com>
2015-12-02 16:28:12 -0800

    add ngstorage but need to inject  & , update

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.directives', 'ionic.ion.showWhen'])
+angular.module('starter', ['ionic', 'ngCordova', 'ngStorage','starter.controllers', 'starter.directives', 'ionic.ion.showWhen'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
