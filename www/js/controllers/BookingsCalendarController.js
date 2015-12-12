c7487a40e3e8442d344d5c6605fa6d8faab09eb7
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-12-11 16:35:43 -0800

    update current-trip in side menu to connect to correct state with saved information:

--- www/js/controllers/BookingsCalendarController.js
+++ www/js/controllers/BookingsCalendarController.js
@@ -3,11 +3,11 @@
 
   angular.module('starter').controller('BookingsCalendarCtrl',
     function($scope, UserService, BookingsService) {
-  // T O D O ///////////    
+  // T O D O ///////////
   // DATE CLICK EVENT - not working will show booking details
       function showBookingDetails(date, jsEvent, view) {
         alert('yo?');
-        alert(date);//pass bookingEvent data here?
+        console.log(date);//pass bookingEvent data here?
       }
       //source code
       $scope.calendarOptions = {
@@ -17,13 +17,13 @@
         },
       };
   /////////////////////////////////
-//**3**take bookings 
+//**3**take bookings
       function prepareBookingEvents(bookings) {
         for (var i = 0; i < bookings.length; i++) {
           var bookingDate = new Date(bookings[i].departing_date);
           //**7**
           $scope.bookingEvents.push({ //bookingEvents holds all bookings
-          //**8** reference code from docs http://angular-ui.github.io/ui-calendar/  
+          //**8** reference code from docs http://angular-ui.github.io/ui-calendar/
             title: 'Trip',
             //**10** add Date API --dates were not displaying: format fullCallendar understands dates
             start: new Date(bookingDate.getFullYear(), bookingDate.getMonth(), bookingDate.getDate())
@@ -43,14 +43,14 @@
 
         BookingsService.driver_id = UserService.driver.id;//**2**from BookingController.js - Line 20
         BookingsService.getBookings().then(function(response) { //from line 21(returns promise)
-          //**9** if 
-          if(response){//API call returns true we put in bookings var 
-        var bookings = BookingsService.bookings;//**5** save bookings object 
+          //**9** if
+          if(response){//API call returns true we put in bookings var
+        var bookings = BookingsService.bookings;//**5** save bookings object
           }
           //call the functions on the bookings we got back
           prepareBookingEvents(bookings);//**6** pass bookings here
         })
-        //when done add bookings events to the event sources [] -- 
+        //when done add bookings events to the event sources [] --
         $scope.eventSources.push($scope.bookingEvents);//push to EventSources array
 
       })
--- www/js/services/loginService.js
+++ www/js/services/loginService.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('starter').service('LoginService', function($q, UserService, $http, $ionicPopup, $state) {
-    //**2** 
+    //**2**
     var self = this; //initiating loginService inside here its self
 
     self.login = function(userLogin, userPw) { //line 27 from longinController.. -
@@ -10,15 +10,16 @@
       //**1**moved from LoginController
       $http.get("http://localhost/apinew/login" + "/" + userLogin + "/" + userPw)
         .success(function(result) {
-          if (!result.result) { //result is return boolean of the API 
+          console.log(result);
+          if (!result.result) { //result is return boolean of the API
             // if (result === '') { *did not work*//
             deferred.resolve(false);
             return false; //no UX message in Services, Best to put in controllers!
           }
-          console.log(result);
           UserService.save(result.data); //**added`.data` login user here**data is the data the API returns (User Object)
           console.log("Login Credentials Submitted Succesfully!");
           deferred.resolve(true);
+          return true;
         }).error(function(data) {
           alert('Something went wrong');
           deferred.resolve(data);
@@ -28,7 +29,7 @@
     }; //login()
 
 
-    //**use logout function in menu.html & controller.js 
+    //**use logout function in menu.html & controller.js
     self.logout = function() {
       var confirmPopup = $ionicPopup.confirm({
         title: 'EXIT?',
