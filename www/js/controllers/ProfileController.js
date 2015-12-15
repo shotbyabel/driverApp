aa3dc199dacd38ded0eeeb6fb386828331e3c1bb
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-12-15 15:01:26 -0800

    create forms for update, and profileService.js

--- www/js/controllers/ProfileController.js
+++ www/js/controllers/ProfileController.js
@@ -3,8 +3,30 @@
  'use strict';
 
   angular.module('starter')
-    .controller('ProfileCtrl', function ($scope, $http, $state, UserService) {
-      console.log(UserService);
+    .controller('ProfileCtrl',
+      function ($scope, $http, $state, UserService, profileService, $ionicPopup) {
+      $scope.user = UserService;
+
+      $scope.profUpdateForm = {}
+
+      $scope.onSubmit = function() {
+        var fullName = $scope.profUpdateForm.Fullname;
+        console.log(fullName);
+      }
+
+      $scope.updateConfirm = function(form) {
+        var endTripPopup = $ionicPopup.confirm({
+          title: "Are you sure?",
+          template: "You are about to Update your Profile."
+        });
+        endTripPopup.then(function(res) {
+          if (res) {
+            $scope.onSubmit(form)
+          }
+        })
+      }
+
+
   });
 
 
---
+++ www/js/services/profileService.js
@@ -0,0 +1,13 @@
+(function() {
+ 'use strict';
+ angular.module('starter')
+ .service('profileService', function($http, $q) {
+
+    var self = this;
+    console.log(self);
+
+    });
+
+
+ // IIFE START //
+})();
