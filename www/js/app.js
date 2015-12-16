4e3c66a4065c633e14fc11b570c9c936b8db2c3e
kimo5532 <shotbyabel@gmail.com>
2015-12-16 14:24:30 -0800

    remove whitelist plugin fix emulator and set up SMS for testing

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ngCordova', 'ngStorage', 'ui.calendar', 'ionic.service.core',
   'ionic.service.push', 'starter.controllers', 'ionic.ion.showWhen'])
-
+///
 .run(function($ionicPlatform, $rootScope, UserService) { //inject $rootScope, UserService in order to do route auth..
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -51,6 +51,15 @@ angular.module('starter', ['ionic', 'ngCordova', 'ngStorage', 'ui.calendar', 'io
   });
   ///////////// $stateChangeStart route auth - end
 })
+
+//identify PushNotification @ start up with ionic.io.service
+// .config(['$ionicAppProvider', function($ionicAppProvider) {
+//   $ionicAppProvider.identify({
+//     app_id: 'b58207b0',
+//     api_key: '984d80a1a9857fc14075de09405f8f1551f7dbacffd7faba',
+//     dev_push: true//only development PUSH notifications - need to set for production later
+//   });
+// }])
 
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
