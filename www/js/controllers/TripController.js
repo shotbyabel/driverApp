56469845a3225cf2abec684a6f2f7c05092adf00
kimo5532 <shotbyabel@gmail.com>
2016-01-02 21:08:09 -0800

    test android SMS

--- www/js/controllers/TripController.js
+++ www/js/controllers/TripController.js
@@ -98,9 +98,9 @@
           var options = {
             replaceLineBreaks: false, // true to replace \n by a new line, false by default
             android: {
-              // intent: '' // send SMS with the native android SMS messaging
-              intent: '' // send SMS without open any other app
-                // intent: 'INTENT' // send SMS inside a default SMS app
+    
+              intent: ''        // send SMS without open any other app
+           // intent: 'INTENT' // send SMS with the native android SMS messaging
             }
           };
           console.log('Passenger Tel:' + $scope.currentCustomer[0].phone);
